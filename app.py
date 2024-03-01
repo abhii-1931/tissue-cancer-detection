08e3adb37402f2659c798dedf19e83e39a2dd293
abhishek choudhary <102737171+abhii-1931@users.noreply.github.com>
2024-03-01 11:01:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,23 +11,23 @@ from io import BytesIO
 app = Flask(__name__)
 
 # Load the pre-trained model using TensorFlow.js
-model = tf.keras.models.load_model("project/model.h5")
+model = tf.keras.models.load_model("model.h5")
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", result="")
+    return render_template("templates/index.html", result="")
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
     if "image" not in request.files:
-        return render_template("index.html", result="Error: No file provided")
+        return render_template("templates/index.html", result="Error: No file provided")
 
     image_file = request.files["image"]
 
     if image_file.filename == "":
-        return render_template("index.html", result="Error: No file selected")
+        return render_template("templates/index.html", result="Error: No file selected")
 
     # Read image file and perform prediction
     # Read image file and perform prediction
@@ -52,7 +52,7 @@ def predict():
 
     # Display the result
     output = "Cancer detected" if prediction > 0.5 else "No cancer detected"
-    return render_template("index.html", result=result, percentage=prediction)
+    return render_template("templates/index.html", result=result, percentage=prediction)
 
 
 if __name__ == "__main__":
